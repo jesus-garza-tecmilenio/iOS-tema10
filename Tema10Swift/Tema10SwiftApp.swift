95e7de49ab9dae9d5a8d47d2a2e705c78ebff212
jesus-garza-gro <jesus.garza.gro@hotmail.com>
2025-10-21 19:43:24 -0600

    Initial Commit

---
+++ Tema10Swift/Tema10SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tema10SwiftApp.swift
+//  Tema10Swift
+//
+//  Created by JESUS GARZA on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Tema10SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
